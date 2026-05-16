6a0642dbfb2734ad2ba89ee6b0e08fcf469d6d86
marceloandrad3 <64045898+marceloandrad3@users.noreply.github.com>
2026-05-16 12:18:54 -0300

    feat: página /app com 3 formas de acesso

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,18 +3,11 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '@/pages/Home'
 import Privacidade from '@/pages/Privacidade'
 import Termos from '@/pages/Termos'
+import AppPage from '@/pages/App'
 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/privacidade',
-    element: <Privacidade />,
-  },
-  {
-    path: '/termos',
-    element: <Termos />,
-  },
+  { path: '/', element: <HomePage /> },
+  { path: '/privacidade', element: <Privacidade /> },
+  { path: '/termos', element: <Termos /> },
+  { path: '/app', element: <AppPage /> },
 ])
